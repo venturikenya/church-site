325cfae8e34d683fe8fa81b3c469176a09a0b5d1
victor <victormungai97@gmail.com>
2017-12-23 16:44:35 +0300

    Victor's commit on contacts section

--- src/map.js
+++ src/map.js
@@ -1,11 +1,18 @@
 function initMap() {
-  var church = {lat: -1.283572, lng: 36.759583};
-    var map = new google.maps.Map(document.getElementById('map'),{
-        zoom: 10,
-        center: church
-    });
-    var marker = new google.maps.Marker({
-        position: church,
-        map: map
-    });
+  var lifegroup = {lat: -1.283572, lng: 36.759583};
+  var church = {lat: -1.285055, lng: 36.757901};
+  var myOptions = {
+    zoom: 10,
+    center: church
+  }
+  var map = new google.maps.Map(document.getElementById('map'), myOptions);
+  var marker = new google.maps.Marker({
+      position: lifegroup,
+      map: map
+  });
+  var map2 = new google.maps.Map(document.getElementById("church"), myOptions);
+  var marker2 = new google.maps.Marker({
+      position: church,
+      map: map2
+  });
 }
